2604aa75c3aba0ba1c2d2d9dc5caac7279c3dfc8
岩本　悠虎 <hakuto1999@outlook.jp>
2020-02-05 11:26:36 +0900

    playerの追従カメラ作成完了

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+   public GameObject Player;
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,13 @@ public class Test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 playerPos = Player.transform.position;
+
+        playerPos.x = Player.transform.position.x;
+        playerPos.y = Player.transform.position.y ;
+        playerPos.z = Player.transform.position.z ;
+
+        transform.position = new Vector3(playerPos.x, playerPos.y, -10);
+
     }
 }
