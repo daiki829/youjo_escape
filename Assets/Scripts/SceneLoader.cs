157e2a0f88d3bb56f5b648b57adf8c65196d56b8
Leus829 <daiki829mail@gmail.com>
2020-02-06 23:46:13 +0900

    シーン切り替え機能追加

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+	[SerializeField]
+	private string scene_name_;
+
+	[SerializeField]
+	private KeyCode keycode_;
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(keycode_))
+		{
+			SceneManager.LoadScene(scene_name_);
+		}
+	}
+
+	public void ChangeScene()
+	{
+		SceneManager.LoadScene(scene_name_);
+	}
+}
